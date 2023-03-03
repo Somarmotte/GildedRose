d2f3034a83d9c18f714dd3046ee80ece04e17171
Marceau Julieron <121799919+Somarmotte@users.noreply.github.com>
2023-03-03 14:20:09 +0100

    Add files via upload

--- GuildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GuildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+
 class GildedRose {
     Item[] items;
 
@@ -13,7 +14,12 @@ class GildedRose {
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+                        if (items[i].getClass() == ItemConjured.class){
+                            items[i].quality = items[i].quality - 2;
+                        }
+                        else {
+                            items[i].quality = items[i].quality - 1;
+                        }
                     }
                 }
             } else {
--- GuildedRose/src/main/java/com/gildedrose/ItemConjured.java
+++ GuildedRose/src/main/java/com/gildedrose/ItemConjured.java
@@ -2,7 +2,6 @@ package com.gildedrose;
 
 public class ItemConjured extends Item{
 
-    public boolean isConjured = true;
     public ItemConjured(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
     }
