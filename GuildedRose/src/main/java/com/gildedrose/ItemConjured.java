17ab4a2355b5b1224872004e070d101d1223ebd3
Marceau Julieron <121799919+Somarmotte@users.noreply.github.com>
2023-03-03 15:09:33 +0100

    Add files via upload

---
+++ GuildedRose/src/main/java/com/gildedrose/ItemConjured.java
@@ -0,0 +1,10 @@
+package com.gildedrose;
+
+public class ItemConjured extends Item{
+
+    public ItemConjured(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+
+}
